d220d1162cbfcfdb0c99560f6eebfc0a02b985a2
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-02 12:24:21 +0100

    fibonacci

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Algorithm");
+//Fibonacci Algorithm
+
+function fibonacci(n) {
+  const fib = [0, 1];
+  for (let i = 2; i < n; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+  return fib;
+}
+ console.log(fibonacci(2));
+console.log(fibonacci(3));
+console.log(fibonacci(7));
