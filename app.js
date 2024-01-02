eaaa484944a43c66ab26415a3518b23acc86c756
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-02 13:58:57 +0100

    power of two

--- app.js
+++ app.js
@@ -42,8 +42,30 @@ function primeNumber(n){
   }
   return true
 }
-console.log("Prime Number.")
+console.log("*****Prime Number.****")
 console.log(primeNumber(1))
 console.log(primeNumber(5))
 console.log(primeNumber(4))
 //  Big-O = O(n) linear time complexity
+
+
+// is Power of two
+function isPowerOfTwo(n){
+  if(n < 1){
+    return false
+  }
+  while(n > 1){
+    if(n % 2 !== 0){
+      return false
+    }
+    n= n / 2
+  }
+  return true
+}
+
+console.log("*****is Power of Two.****");
+console.log(isPowerOfTwo(1));
+console.log(isPowerOfTwo(2));
+console.log(isPowerOfTwo(5));
+// Big-O = Olog(n)
+
