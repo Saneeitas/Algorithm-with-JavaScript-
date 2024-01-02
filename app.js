9f981e10d9a187cfa8fafc7636f1a774dc2a2557
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-02 13:07:02 +0100

    prime number

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ console.log("****Fibonacci****");
 console.log(fibonacci(2));
 console.log(fibonacci(3));
 console.log(fibonacci(7));
-//  Big-O = O(n) linear
+//  Big-O = O(n) linear time complexity
 
 function factorial(n){
   let result = 1;
@@ -27,4 +27,23 @@ console.log("****Factorial****")
 console.log(factorial(0))
 console.log(factorial(1))
 console.log(factorial(5))
-//  Big-O = O(n) linear
+//  Big-O = O(n) linear time complexity
+
+
+// Prime numbers
+function primeNumber(n){
+  if(n < 2){
+    return false
+  }
+  for(let i = 2; i <= Math.sqrt(n); i++){
+    if(n % i === 0){
+      return false
+    }
+  }
+  return true
+}
+console.log("Prime Number.")
+console.log(primeNumber(1))
+console.log(primeNumber(5))
+console.log(primeNumber(4))
+//  Big-O = O(n) linear time complexity
