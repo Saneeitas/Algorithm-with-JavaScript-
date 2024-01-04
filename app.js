10307df3f46d72d7fead480d2bd5188cc9b706a2
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-04 22:56:20 +0100

    bubble sort

--- app.js
+++ app.js
@@ -183,3 +183,24 @@ console.log(recursiveBinarySearch([-5,2,4,6,10],20))
 
 
 // Sorting Algorithm
+
+// Bubble Sort
+function bubbleSort(arr){
+  let swapped;
+  do{
+    swapped = false;
+    for(let i = 0; i < arr.length - 1; i++){
+      if(arr[i] > arr[i+1]){
+        let temp= arr[i];
+        arr[i] = arr[i+1]
+        arr[i+1] = temp
+        swapped = true
+      }
+    }
+  }while(swapped)
+}
+console.log("***Bubble Sort***");
+const arr = [8,20,-2,4,-6]
+bubbleSort(arr);
+console.log(arr)
+// Big-O = O(n^2)
