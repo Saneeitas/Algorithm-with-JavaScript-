a9a5936d5907c03f5b680f87c16b43ac114cc227
Muhammad Sani Ibrahim <87580367+Saneeitas@users.noreply.github.com>
2024-01-04 22:32:04 +0100

    sorting algorithm

--- app.js
+++ app.js
@@ -180,3 +180,6 @@ console.log(recursiveBinarySearch([-5,2,4,6,10],10))
 console.log(recursiveBinarySearch([-5,2,4,6,10],6))
 console.log(recursiveBinarySearch([-5,2,4,6,10],20))
 // Big-O = O(logn)
+
+
+// Sorting Algorithm
